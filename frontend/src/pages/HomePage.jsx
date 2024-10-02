2e7217d97bca2840bb41c314ee35cfcee4ba1289
thestarharsh <talatiharsh19@gmail.com>
2024-10-02 18:06:54 +0530

    Front End Developed

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react'
+import { useProductStore } from '../store/product'
+import { ProductCard } from '../components/ProductCard'
+
+
+export const HomePage = () => {
+    const { products, getProducts } = useProductStore();
+    useEffect(() => {
+        getProducts();
+    }, [getProducts]);
+    console.log(products);
+
+    return (
+        <Container maxW='container.x1' py={12}>
+            <VStack spacing={8}>
+                <Text
+                    fontSize={"30"}
+                    fontWeight={"bold"}
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip={"text"}
+                    textAlign={"center"}
+                >
+                    Current Products 🚀
+                </Text>
+
+                <SimpleGrid
+                    columns={{
+                        base: 1,
+                        md: 2,
+                        lg: 3
+                    }}
+                    spacing={10}
+                    w={"full"}
+                >
+                    {products.map((product) => (
+                        <ProductCard key={product._id} product={product} />
+                    ))}
+                </SimpleGrid>
+
+                {products.length === 0 && (
+                    <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+                        No products found 😢{" "}
+                        <Link to="/create">
+                            <Text as='span' color='blue.500'>
+                                Create a product
+                            </Text>
+                        </Link>
+                    </Text>
+                )}
+            </VStack>
+        </Container>
+    )
+}
